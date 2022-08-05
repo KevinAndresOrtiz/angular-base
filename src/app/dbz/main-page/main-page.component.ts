e7b534c28231ce6ad00e23668f83f0b7f5b678cb
Kevin Ortiz Merchan <kevin.ortiz@globant.com>
2022-08-05 16:04:26 -0500

    added characters bdz

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15,
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 1000
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 1000
   }
+
+  agregarNuevoPersonaje( personaje: Personaje ) {
+    this.personajes.push(personaje)
+
+  }
+
+
 
 }
